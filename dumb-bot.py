31ac6eaf7486bb892a18cf2df6d6908adcb90870
Steven Leibrock <steven.leibrock@gmail.com>
2016-10-03 13:21:53 -0400

    Putting in some temporary fun stuff

--- dumb-bot.py
+++ dumb-bot.py
@@ -147,6 +147,37 @@ async def yt(*search):
         logger("Fail: {}".format(ex))
     return await client.say("Failed to request the search")
 
+@client.command()
+async def goodshit(*args):
+    """
+    Stupid command that is temporary
+    """
+    data = [":ok_hand::eyes::ok_hand::eyes::ok_hand::eyes::ok_hand::eyes::ok_hand::eyes: ",
+            "good shit go౦ԁ sHit:ok_hand: thats :heavy_check_mark: some good:ok_hand:",
+            ":ok_hand:shit right:ok_hand::ok_hand:there:ok_hand::ok_hand::ok_hand: right",
+            ":heavy_check_mark:there :heavy_check_mark::heavy_check_mark:if i do ƽaү so my ",
+            " :100: i say so :100: thats what im talking about right there right there ",
+            "(chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ:100: :ok_hand::ok_hand: :ok_hand:НO0ОଠOOOOOОଠଠ",
+            "Ooooᵒᵒᵒᵒᵒᵒᵒᵒᵒ:ok_hand: :ok_hand::ok_hand: :ok_hand: :100: :ok_hand: :eyes:",
+            " :eyes: :eyes: :ok_hand::ok_hand:Good shit"]
+            
+    return await client.say("".join(data))
+
+@client.command()
+async def osfrog(*args):
+    data = [":frog: BALANCE IN ALL THINGS :frog:",
+            ":thumbsup: Thanks Purge :thumbsup:",
+            ":frog: :gun:",
+            ":frog: le balanced :fish: man :frog:",
+            ":frog: +1 :shield: armor :frog:",
+            ":frog: :dragon: Illusory orb speed increased by 1 :frog:",
+            ":frog: Vacuum​ cooldown rescaled from 28.0 seconds to 28 seconds :frog:",
+            ":frog: le balanced :cloud_tornado: man :frog:",
+            ":frog: SEEMS GOOD TO ME :frog:",
+            ":frog: le balanced 1050 :dragon: lance attack range :frog:"]
+    return await client.say(choice(data))
+            
+
 if __name__ == "__main__":
     try:
         argv.pop(0)

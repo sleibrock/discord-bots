bf8a669c5b3dae3988c14188fb664a0d52fa8631
Steven Leibrock <steven.leibrock@gmail.com>
2016-12-05 13:59:05 -0500

    Reverting supervisor and tests

--- src/supervisor.rkt
+++ src/supervisor.rkt
@@ -1,25 +1,28 @@
 #lang racket/base
 
-(require racket/system)
-(provide bots total-bots main syscall)
+(require racket/system racket/list)
+(provide
+ bots
+ total-bots
+ subproc
+ subproc-kill
+ start-bot
+ reviver
+ main
+ )
 
-;; Shortcut to do syscalls with multiple arguments
-;; syscall :: (Listof String) -> Boolean
-(define (syscall cmds)
-  (parameterize ([current-subprocess-custodian-mode 'kill])
-    (apply system* (append (list (find-executable-path (car cmds))) (cdr cmds)))))
-
-;; bots :: (Vectorof (Listof String))
+;; Define bots here - file to execute, keys, colors for logging
 (define bots
-  (list '("python"    "src/dumb-bot.py")
-        '("python"   "src/graph-bot.py")
-        '("python"  "src/hacker-bot.py")
-        '("python" "src/janitor-bot.py")
-        '("node"   "src/FishFactBot.js")))
-(define total-bots (length bots))
+  (vector
+    (list "python"     "src/dumb-bot.py")
+    (list "python"    "src/graph-bot.py")
+    (list "python"   "src/hacker-bot.py")
+    (list "python"  "src/janitor-bot.py")
+    (list "node"    "src/FishFactBot.js")
+    ))
+(define total-bots (vector-length bots)) ; store how many have been set up
 
 ;; Creates a new function which prints out a message with a name attached
-;; set-logger :: String -> String -> String -> Void
 (define (set-logger t-name color)
   (λ (str)
     (define cd (seconds->date (current-seconds))) 
@@ -27,26 +30,45 @@
      (apply (λ (x y z) (format "[\033[38;5;~am~a\033[0m @ ~a:~a:~a] ~a" color t-name x y z str))
             (map (λ (i) (if (< i 10) (format "0~a" i) (format "~a" i)))
                  (list (date-hour cd) (date-minute cd) (date-second cd)))))))
-(define logger (set-logger "supervisor  " 1))
 
-;; Create a function which will yield new threads that send data to a parent thread
-;; bot-factory :: Thread -> Nonnegative-Integer -> Thread
-(define (bot-factory parent-thread)
-  (λ (bot-item)
-    (thread
-     (λ ()
-       (logger (format "Starting Bot ~a" (car (cdr bot-item))))
-       (syscall bot-item) 
-       (thread-send parent-thread bot-item)))))
+(define logger (set-logger "supervisor  " 9))
+
+;; a quick wrapper function for subprocess 
+(define (subproc p a)
+  (parameterize ([current-subprocess-custodian-mode 'kill])
+    (apply
+     subprocess
+     (append (list (current-output-port) #f 'stdout (find-executable-path p)) a))))
+
+;; A one-arg subprocess-kill wrapper
+(define (subproc-kill subp)
+  (subprocess-kill subp #t))
+
+;; Create a new subprocess and return it
+(define (start-bot bot-id)
+  (define-values (interp code) (apply values (vector-ref bots bot-id)))
+  (define-values (a b c d)
+    (subproc interp (list code)))
+  (logger (format "Initialized Bot ~a on PID ~a" bot-id (subprocess-pid a)))
+  a)
+
+;; Revive a subprocess as required
+(define (reviver threads)
+  (λ (bot-id)
+    (define subp (vector-ref threads bot-id))
+    (when (not (eqv? 'running (subprocess-status subp)))
+      (subprocess-kill subp #t)
+      (vector-set! threads bot-id (start-bot bot-id)))))
 
 ;; Gravekeeper main function to keep the subprocesses running
-;; main :: Void
 (define (main)
-  (logger "Starting bots")
-  (define start-bot (bot-factory (current-thread)))
-  (for-each start-bot bots)
+  (define threads (build-vector total-bots start-bot))
+  (define revive (reviver threads))
+  (logger "Gravekeeper thread initialized")
+  (sleep 30) ; wait for bots to start before looping
   (define (loop)
-    (start-bot (thread-receive))
+    (for-each revive (range total-bots))
+    (sleep 300) ; number of seconds the gravekeeper should sleep
     (loop))
   (loop))
 
--- tests.rkt
+++ tests.rkt
@@ -1,32 +1,53 @@
 #lang racket
 
-(require "src/supervisor.rkt")
-
-(module+ test (require rackunit racket/port))
-
-(module+ test
-  (define (debug-args bot)
-    (match (car bot)
-      ("node"   (cons (car bot) (append '("-c") (cdr bot))))
-      ("python" (cons (car bot) (append '("-m" "py_compile") (cdr bot))))))
-  (define (sys->str cmd)  (with-output-to-string (λ () (syscall cmd)))))
-                          
-(module+ test
-  (test-case "Test whether all packages have been installed"
-    (let ([pip-out (sys->str '("pip" "freeze" "--disable-pip-version-check"))]
-          [req-txt (port->lines (open-input-file "requirements.txt"))])
-      (displayln "Checking if packages were installed...")
-      (check <=
-             (length req-txt)
-             (length (filter non-empty-string? (string-split pip-out "\n")))))))
+(module+ test (require rackunit racket/port "src/supervisor.rkt"))
 
 (module+ test 
-   (test-case "Testing successful compiles on all bots"
-     (let ([subs (map (λ (bot) (syscall (debug-args bot))) bots)])
-       (displayln "Checking if all bots were properly tested...")
-       (check = (length subs) total-bots)
-       (displayln "Checking if all bots compiled successfully...")
-       (for-each check-true subs)
-       )))
+    (define (debug-args program code)
+      (match program
+        ("node"   (list "-c" code))
+        ("python" (list "-m" "py_compile" code))))
+    (define (syscall cmds)
+      (parameterize ([current-subprocess-custodian-mode 'kill])
+        (apply system* (append (list (find-executable-path (car cmds))) (cdr cmds)))))
+    (define (sys->str cmd) (with-output-to-string (λ () (syscall cmd)))))
 
+(module+ test
+   (test-case "Testing ability to kill subprocs"
+     (let ([subs
+            (build-list
+             10
+             (lambda (x)
+                 (define-values (a b c d)
+                   (subproc "sleep" '("300000")))
+                     a))])
+       (check = (length subs) 10 "Test size not equal")
+       (for-each subproc-kill subs)
+       (sleep 2) ; sleep to wait on the OS process killing
+       (for-each
+        (lambda (sub)
+          (check-not-eq? 'running (subprocess-status sub)))
+        subs)))
+   
+   (test-case "Testing successful compiles on all bots"
+     (let ([subs
+            (build-list
+             total-bots
+             (lambda (x)
+                 (define-values (interp code) (apply values (vector-ref bots x)))
+                 (define-values (a b c d) (subproc interp (debug-args interp code)))
+                 a))])
+       (check = (length subs) total-bots)
+       (sleep 5)
+       (for-each (lambda (sub) (check-eq? 0 (subprocess-status sub))) subs)
+       0))
+
+   (test-case "Test whether all packages have been installed"
+     (let ([pip-out (sys->str '("pip" "freeze" "--disable-pip-version-check"))]
+           [req-txt (port->lines (open-input-file "requirements.txt"))])
+       (displayln "Checking if packages were installed...")
+       (check <=
+              (length req-txt)
+              (length (filter non-empty-string? (string-split pip-out "\n"))))))
+   )
 ; end

68f17b7b9fbd90f5ebb6a0390386b6fc5831af58
S Leibrock <steven.leibrock@gmail.com>
2017-03-31 16:59:25 -0400

    Updating dota-bot with more

--- bots/dota-bot.py
+++ bots/dota-bot.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+"""
+Webhook automated Dota 2 match parsing service for Discord
+
+"""
+
 from json import load as jload, dump as jdump
 from time import sleep
 from botinfo import *
@@ -10,13 +15,28 @@ from pathlib import Path
 DELAY_TIMER = 10 # 60 * 30
 INBETWEEN_TIMER = 30
 
+# Web hook key (this should be read from a keyfile)
 WEB_HOOK = "https://discordapp.com/api/webhooks/296303788057427969/CUr6cezgCmIjYfVrbcxTIu2HnFKSorOUv15DKwnOoEvJcTFx8ZMNRR5RhQyNCqodHCHI"
 
 OPENDOTA_API = "https://api.opendota.com/api"
 OPENDOTA_URL = "https://opendota.com/matches"
 
-HERO_DATA_CACHED = False
+filegen = create_filegen("shared")
+
 HERO_DATA_PATH   = Path("shared/heroes.json")
+
+# Load the cache (either from file or fresh JSON query)
+if HERO_DATA_PATH.is_file():
+    with open(HERO_DATA_PATH, "r") as f:
+        HERO_DATA = jload(f)
+else:
+    r = get(f"{OPENDOTA_API}/heroes")
+    if r.status_code != 200:
+        logger(f"Failed to pre-fetch Hero JSON data from openDota")
+        quit()
+    HERO_DATA = r.json()
+    with open(HERO_DATA_PATH, "w") as f:
+        jdump(HERO_DATA, f)
 
 SHARED = Path("shared")
 logger = create_logger("dota-bot")
@@ -34,6 +54,10 @@ def get_payload(dota_id, match_id):
     radiant_score = jsonblob["radiant_score"]
     dire_score = jsonblob["dire_score"]
     game_mode = jsonblob["game_mode"]
+    duration = jsonblob["duration"]
+
+    minutes = str(duration // 60)
+    seconds = str(duration % 60).zfill(2)
 
     player = None
     for p in jsonblob["players"]:
@@ -44,23 +68,67 @@ def get_payload(dota_id, match_id):
         logger("Failed to find the player")
         return False
 
-    # Score field
+    # Player variable declarations for use later
+    player_team = player["isRadiant"]
+    pname = player["personaname"] 
+    hero_name = "LOL"
+
+    # Score of game
     embs.append({
-        "name": "Score (KDA)",
-        "value": f"{player['kills']}/{player['deaths']}/{player['assists']}",
+        "name": "Final Score",
+        "value": f"Radiant **{radiant_score}** - **{dire_score}** Dire",
         "inline": True
     })
 
+    # Player Stats field
+    if player_team:
+        percent_of_team = round(((player["kills"]+player["assists"]) / float(radiant_score)) * 100.0, 2)
+    else:
+        percent_of_team = round(((player["kills"]+player["assists"]) / float(dire_score)) * 100.0, 2)
+            
+    embs.append({
+        "name": "Stats (KDA)",
+        "value": f"{player['kills']}/{player['deaths']}/{player['assists']} ({percent_of_team}% involvement)",
+        "inline": True
+    })
+
+    # GPM / XPM
+    embs.append({
+        "name": "GPM / XPM",
+        "value": f"{player['gold_per_min']} / {player['xp_per_min']}",
+        "inline": True
+    })
+
+    # Replay sensitive data follows after this comment
+    # OpenDota takes time to parse the replay of each match, such that more info can be gathered
+    # from the match. Things like chat, pings, runes and damage instances are all based on replays
+    # If this stuff isn't immediately available, it shouldn't be added to the Embed
+    # ping details
+    if 'pings' in player:
+        total_pings = sum((p["pings"] for p in jsonblob["players"] if p["isRadiant"] == player_team))
+        pingpc = round((float(player["pings"]) / total_pings) * 100.0, 2)
+        embs.append({
+            "name": "Total Pings",
+            "value": f"{player['pings']} ({pingpc}% of team)",
+            "inline": True
+        })
+
+    # rune details
+    if 'runes' in player:
+        pass
+
+
     # craft the main embed
+    winstatus = "won" if player_team == radiant_win else "lost"
     data["embeds"] = [{
         "title": f"Results for Match #{match_id}",
-        "description": "something something player victory status",
+        "description": f"{player['personaname']} {winstatus} as {hero_name} ({minutes}:{seconds})",
         "url": f"{OPENDOTA_URL}/{match_id}",
-        #"color": int(match_id % 0xffffff),
-        #"fields": embs,
-        #"footer": {
-        #    "text": "Provided by OpenDota API"
-        #}
+        "color": int(match_id % 0xffffff),
+        "fields": embs,
+        "footer": {
+            "text": "Provided by OpenDota API"
+        }
     }]
     return data
 
@@ -90,21 +158,13 @@ def fetch_last_match(path):
     if match_id <= cache_v:
         logger(f"{path}: Latest match not newer than cached match")
         return False
-
-    """
-    Test this blob
     
-    {"embeds":[{"title":"Results for Match 3086034297","description":"Ste5e lost as Bristleback","url":"https://opendota.com/matches/3086034297","color":15803952,"footer":{"url":"http://cdn.dota2.com/apps/dota2/images/nav/logo.png","width":167,"height":33},"fields":[{"name":"K/D/A","value":"4/6/11","inline":true},{"name":"GPM / XPM","value":"418 / 645","inline":true},{"name":"Pings Used","value":"55 (49.11% of team)","inline":true},{"name":"Bounty Runes Collected","value":"6 (9.23% of all bounties)","inline":true},{"name":"Longest Chat","value":"'these 2 are fuckin kiddin me' -Ste5e","inline":true}]}]}
-    """
-    
-    print(data)
     payload = get_payload(dota_id, match_id)
-    print(payload)
-    res = post(WEB_HOOK, payload)
-    print(res)
-    post(WEB_HOOK, {"embeds": [{"title":"Test","description": "test description", "url":"https://google.com/","color":200, "fields":[{"name":"test","value":"test message", "inline":True}]}]})
-    post(WEB_HOOK, {"content": "test"})
-    return
+    if not payload:
+        logger("Failed to parse data for M#{match_id} (P#{dota_id})")
+        return False
+    res = post(WEB_HOOK, json=payload, headers={"content-type": "application/json"})
+    return res
 
 def main():
     logger("Beginning Match Parsing...")
